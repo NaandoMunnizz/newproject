f42187e58710c644f5ff2e4565b5404dd3213e2f
NaandoMunnizz <139184906+NaandoMunnizz@users.noreply.github.com>
2023-07-10 21:11:19 -0300

    criando repositório de teste.
    Estudando: JavaScript, Html5, Css3

---
+++ Onde/myScript.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML =
+        "Paragrafo mudou.";
+}
+
+function myFunction() {
+    document.getElementById("demo2").innerHTML =
+        "FERNANDO MUNIZ .";
+}
